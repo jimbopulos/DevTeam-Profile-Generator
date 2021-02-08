90b085186fa46029c3c7aae71d6f46f3786e07d5
James Galantino <james_galantino@aol.com>
2021-02-07 22:14:11 -0500

    Added test parameters to employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,15 @@
-const Employee = require('./lib/Employee');
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+    describe("Initialize", () => {
+        // should have a full name, id, and email
+        it("should have a full name, id, email", () => {
+            // arrange/act
+            const employee = new Employee("Tom", "Riddle", "holcrux@darkmagic.com");
+            // assert 
+            expect("fullName" in employee).toEqual(true);
+            expect("id" in employee).toEqual(true);
+            expect("email" in employee).toEqual(true);
+        })
+    })
+})
