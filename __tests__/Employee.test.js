4e74bcada154233e0ccc6eb424cbcd4e30549281
James Galantino <james_galantino@aol.com>
2021-02-08 17:16:05 -0500

    Made changes to role classes, and employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,29 +3,27 @@ const Employee = require('../lib/Employee');
 describe("Employee", () => {
     describe("Initialize", () => {
         // should have a full name, id, and email
-        it("should have a first name, last name, id, email", () => {
+        it("should have a name, id, email", () => {
             // arrange/act
-            const employee = new Employee("Tom", "Riddle", 4567, "triddle@holcrux.com");
+            const employee = new Employee("Tom Riddle", 4567, "triddle@holcrux.com");
             // assert 
-            expect("firstName" in employee).toEqual(true);
-            expect("lastName" in employee).toEqual(true);
+            expect("name" in employee).toEqual(true);
             expect("id" in employee).toEqual(true);
             expect("email" in employee).toEqual(true);
         })
 
         // should not have a missing value for name
-        it("should not have a missing value for first or last name", () => {
+        it("should not have a missing value name", () => {
             // arrange/act
-            const employee = new Employee("Tom", "Riddle", 4567, "triddle@holcrux.com");
+            const employee = new Employee("Tom Riddle", 4567, "triddle@holcrux.com");
             // assert
-            expect(employee.firstName).not.toEqual(undefined);
-            expect(employee.lastName).not.toEqual(undefined);
+            expect(employee.name).not.toEqual(undefined);
         });
 
         // should have an employee id that is a number, not a string
         it("should have an employee id that is a number, not a string", () => {
             // arrange/act
-            const employee = new Employee ("Tom", "Riddle", 4567, "triddle@holcrux.com");
+            const employee = new Employee ("Tom Riddle", 4567, "triddle@holcrux.com");
             // assert
             expect(typeof employee.id).toEqual("number");
         })
@@ -33,12 +31,19 @@ describe("Employee", () => {
         // should have a valid e-mail address format
         it("should have a valid e-mail address format", () => {
             // arrange/act
-            const employee = new Employee("Tom", "Riddle", 4567, "triddle@holcrux.com")
+            const employee = new Employee("Tom Riddle", 4567, "triddle@holcrux.com")
 
             // assert
             expect(employee.email).toEqual("triddle@holcrux.com");
         })
     })
+
+    // describe("getFirstName", () => {
+    //     // should give the first name of the employee
+    //     it("should give the first name of the employee", () => {
+
+    //     })
+    // })
 
     describe("getRole", () => {
         // should be an employee of the company
